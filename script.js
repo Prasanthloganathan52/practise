537777d74601e34fc64ea2c13335b30f8642306d
admin <prasanthloganathan01@gmail.com>
2024-06-17 15:08:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+   //  var x=5;
+   //  var y=3;
+   //  var sum =x+y
+   //  document.write(sum)
+
+   // array
+   // let cars=["bmw","audi","jagur","volvo","honda"];
+   // console.log(cars[0]);
+   // console.log(cars[1]);
+   // console.log(cars[2]);
+   // console.log(cars[3]);
+   // console.log(cars[4]);
+ 
+   // const fruits =["apple","banana","orange"];
+   // console.log(fruits.includes("banana"));
+""
+// 4
